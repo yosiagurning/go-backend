6e3d41fb2a622d215a45440c92833863ccb1b662
yosiagurning <149059132+yosiagurning@users.noreply.github.com>
2025-09-17 13:44:15 +0700

    coba

--- database/database.go
+++ database/database.go
@@ -15,7 +15,7 @@ var DB *gorm.DB
 // Fungsi untuk menghubungkan ke database
 func ConnectDatabase() {
 	var err error
-	dsn := "root:OEEYcvQBItzCMavRHbNQyWlFkrHXwBxU@tcp(shinkansen.proxy.rlwy.net:25817)/railway?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:@tcp(127.0.0.1:3306)/adminretribusi?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
@@ -24,7 +24,7 @@ func ConnectDatabase() {
 	fmt.Println("✅ Database connected successfully!")
 
 	// Migrasi model ke dalam database
-	err = DB.AutoMigrate(&models.Price{}, &models.Market{}, &models.User{}, &models.Barang{}, &models.BarangHistory{}, &models.PriceHistory{}, &models.Category{}, &models.MarketOfficer{},&models.CategoryMarket{})
+	err = DB.AutoMigrate(&models.Price{}, &models.Market{}, &models.User{}, &models.Barang{}, &models.BarangHistory{}, &models.PriceHistory{}, &models.Category{}, &models.MarketOfficer{}, &models.CategoryMarket{})
 	if err != nil {
 		log.Fatalf("❌ Failed to migrate the database: %v\n", err)
 	}
